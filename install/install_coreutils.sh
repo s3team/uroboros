b567e31c6ff87837b7a3ce89eaab1b7099206414
Giyeol Kim <gpk5233@psu.edu>
2026-01-28 13:47:42 -0500

    add ARM test script for coreutils
    - modify Makefile to delete *.result files

--- install/install_coreutils.sh
+++ install/install_coreutils.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 wget ftp.gnu.org/gnu/coreutils/coreutils-9.6.tar.xz
 tar -xf coreutils-9.6.tar.xz
 cp -r coreutils-9.6 $(pwd)/coreutils-32bit
 cp -r coreutils-9.6 $(pwd)/coreutils-64bit
 cp -r coreutils-9.6 $(pwd)/coreutils-arm-32bit/
+cp -r coreutils-9.6 $(pwd)/coreutils-arm-thumb/
 cp -r coreutils-9.6 $(pwd)/coreutils-arm-64bit/
 
 export CFLAGS="-m32 -no-pie"
 export CXXFLAGS="-m32 -no-pie"
-
 pushd $(pwd)/coreutils-32bit
 ./configure
 make -j 4
@@ -21,8 +23,8 @@ make -j 4
 popd
 
 export CC="arm-linux-gnueabihf-gcc"
-export CFLAGS="-no-pie"
-pushd $(pwd)/coreutils-arm-32bit
+export CFLAGS="-mthumb -no-pie"
+pushd $(pwd)/coreutils-arm-thumb
 ./configure --host=arm
 make -j 4
 popd
---
+++ test/run_coreutils.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import glob
+import os
+import argparse
+import subprocess
+import time
+from pathlib import Path
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJ_ROOT_PATH = str(SCRIPT_DIR.parent)
+
+
+def run(executable_binaries: list[str], arch):
+    for binary in executable_binaries:
+        binary_only_name = os.path.basename(binary)
+        result_path = f"{PROJ_ROOT_PATH}/src/{binary_only_name}.result"
+        print(f"python3 uroboros.py {binary} --arch {arch}", flush=True)
+
+        # If runtime exceed 600 seconds, terminate the process
+        timeout = 600
+        start_time = time.time()
+        try:
+            result = subprocess.run(
+                ["python3", "uroboros.py", binary, "--arch", arch],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                timeout=timeout,
+                cwd=PROJ_ROOT_PATH + "/src",
+            )
+        except subprocess.TimeoutExpired:
+            print(f"Timeout expired: {binary_only_name}")
+            with open(result_path, "w") as f:
+                f.write(f"Timeout expired: {binary_only_name}")
+            continue
+        end_time = time.time()
+
+        # Save result.stdout to /home/gpk5233/work/research/Uroboros_ws/AIL/src
+        with open(result_path, "w") as f:
+            f.write(result.stdout.decode())
+            # Save the time diff
+            time_diff = end_time - start_time
+            f.write(f"Time taken: %.2f\n" % time_diff)
+
+        print(f"Time taken: %.2f" % time_diff)
+
+        # Run qemu-arm -L /usr/arm-linux-gnueabihf/ ~/work/research/Uroboros_ws/AIL/src/a.out --help
+        # and record if it runs successfully or not
+        print("Running qemu-arm to verify the binary...", flush=True)
+        print(
+            f"qemu-arm -L /usr/arm-linux-gnueabihf/ {PROJ_ROOT_PATH}/src/a.out --help",
+            flush=True,
+        )
+        qemu_result = subprocess.run(
+            [
+                "qemu-arm",
+                "-L",
+                "/usr/arm-linux-gnueabihf/",
+                f"{PROJ_ROOT_PATH}/src/a.out",
+                "--help",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        if (binary_only_name != "false" and qemu_result.returncode == 0) or (
+            binary_only_name == "false" and qemu_result.returncode == 1
+        ):
+            print(f"qemu-arm succeeded: {binary_only_name}\n")
+        else:
+            print(f"qemu-arm failed: {binary_only_name}\n")
+
+
+def get_binaries(arch: str, programs: list[str]) -> list[str]:
+    # Set the directory path based on architecture
+    dir_path = ""
+    if arch == "thumb":
+        dir_path = os.path.join(PROJ_ROOT_PATH, "install", "coreutils-arm-thumb", "src")
+    elif arch == "arm":
+        dir_path = os.path.join(PROJ_ROOT_PATH, "install", "coreutils-arm-32bit", "src")
+    elif arch == "aarch64":
+        dir_path = os.path.join(PROJ_ROOT_PATH, "install", "coreutils-arm-64bit", "src")
+
+    # Get all executable binaries in the `dir_path`
+    executable_binaries = glob.glob(os.path.join(dir_path, "*"))
+    executable_binaries = [f for f in executable_binaries if os.access(f, os.X_OK)]
+
+    # Filter binaries if specific programs are provided
+    if programs:
+        executable_binaries = [
+            binary
+            for binary in executable_binaries
+            if os.path.basename(binary) in programs
+        ]
+
+    # Remove excluded binaries from the list
+    exclude_list = ["dcgen", "blake2", "libstdbuf.so"]
+    executable_binaries = [
+        binary
+        for binary in executable_binaries
+        if os.path.basename(binary) not in exclude_list
+    ]
+
+    # Sort the binaries in ascending order on sizes
+    executable_binaries.sort(key=os.path.getsize)
+
+    return executable_binaries
+
+
+def dry_run(uroboros_path: str, executable_binaries: list[str], arch):
+    command = ["python3", uroboros_path, "--arch", arch]
+    for binary in executable_binaries:
+        command.append(binary)
+        print(f"Dry run command: {' '.join(command)}")
+
+
+def main(args):
+    arch = args.arch
+    programs = args.programs
+    print(f"Running tests with architecture: {arch}")
+    executable_binaries = get_binaries(arch, programs)
+
+    # dry_run(uroboros_path, executable_binaries, arch)
+    run(executable_binaries, arch)
+
+
+if __name__ == "__main__":
+    # get arguments options from command line like --arch, -m
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--arch",
+        type=str,
+        default="thumb",
+        help="Architecture to use (default: thumb), options: thumb, arm, aarch64",
+    )
+    # Get binary names to test them only
+    parser.add_argument(
+        "--programs",
+        type=str,
+        nargs="*",
+        help="List of binary names to test (default: all binaries)",
+    )
+    args = parser.parse_args()
+    main(args)
